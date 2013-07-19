45bddf2074c1e28d7e02b474de7b054f862dc41f
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2013-07-19 02:21:51 +0100

    Add db details

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'darts',
+			'username'  => 'darts',
+			'password'  => 'dtwd!#dQ',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
